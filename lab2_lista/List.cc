6ba19f133001381d649b8d5fac2e6e13ebc5c6c1
eripe625 <erik.p.hemming@gmail.com>
2020-02-14 11:02:22 +0100

    Nej inte alls

--- lab2_lista/List.cc
+++ lab2_lista/List.cc
@@ -1,18 +1,23 @@
 #include "List.h"
 using namespace std;
 
+
+Sorted_List::Sorted_List:
+firstptr{nullptr}, lastptr{nullptr}
+{}
+  Sorted_List(element* first, element* last):
+  firstptr{first}, lastptr{last} {}
+/*
 Sorted_List::Sorted_List(int d): data(d), next(NULL), prev(NULL)
 {}
 
-
-/*
 complex::complex(int r, int i):
 real{r}, imag{i}
 
 class complex {
 public:
   complex (int r, int i);
-}
+}stptr{nullptr}, lastp
 
 Sorted_List::Sorted_List(first{nullptr}, last{nullptr}):
 firstptr(first), lastptr{last}
